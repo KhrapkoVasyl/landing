c6510b4332f079f915b5c99481cf86f7c2fc9edc
VasylKhrapko <khrapko2002@gmail.com>
2022-12-20 22:21:02 +0200

    implement responsive design of page

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const mobileNav = document.querySelector('.nav-mobile');
+
+hamburgerBtn.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+});
